5de5a26b15523b8ebf743f2786198f71982c314d
ajaybarath <ajaybarath3@gmail.com>
2021-09-16 09:20:28 +0530

    ADDED: test method to find cheap hotel

--- src/test/java/com/bridgeLabz/hotelReservationSystem/HotelTest.java
+++ src/test/java/com/bridgeLabz/hotelReservationSystem/HotelTest.java
@@ -2,6 +2,10 @@ package com.bridgeLabz.hotelReservationSystem;
 
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Assert;
 
 public class HotelTest {
@@ -13,5 +17,28 @@ public class HotelTest {
 
 		Assert.assertEquals("Lakewood", hotel.getName());
 		Assert.assertEquals(110, hotel.getRate());
+	}
+	
+	
+	public void getCheapHotelTest() throws ParseException {
+		Hotel hotel1 = new Hotel("Lakewood", 110);
+		Hotel hotel2 = new Hotel("RidgeWood", 220);
+		Hotel hotel3 = new Hotel("BridgeWood", 160);
+		
+		HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+		hotelReservationSystem.addHotel(hotel1);
+		hotelReservationSystem.addHotel(hotel2);
+		hotelReservationSystem.addHotel(hotel3);
+		
+		String sDate="16/09/2021";  
+	    Date startDate=new SimpleDateFormat("dd/MM/yyyy").parse(sDate);  
+	    
+	    String eDate="20/09/2021";  
+	    Date endDate=new SimpleDateFormat("dd/MM/yyyy").parse(eDate);  
+
+		Hotel hotel = hotelReservationSystem.findCheapHotels(startDate, endDate);
+		
+		Assert.assertEquals(hotel.getName(), "Lakewood");
+		
 	}
 }
