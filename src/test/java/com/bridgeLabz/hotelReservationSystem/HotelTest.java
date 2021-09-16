c823f494f78528e9d13b3063f888af144cd6c231
ajaybarath <ajaybarath3@gmail.com>
2021-09-16 17:53:13 +0530

    ADDED: test method by getting userInput and give the best rated cheaop hotel

--- src/test/java/com/bridgeLabz/hotelReservationSystem/HotelTest.java
+++ src/test/java/com/bridgeLabz/hotelReservationSystem/HotelTest.java
@@ -6,7 +6,9 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Scanner;
 
 import org.junit.Assert;
 
@@ -73,7 +75,8 @@ public class HotelTest {
 		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 19);
 
 		Hotel cheapHotel = hotelReservationSystem.findCheapHotelsWithWeekDaysAndWeekEndRates(startDate, endDate);
-		System.out.println(cheapHotel.getName() + ", and TotalRates : "  + hotelReservationSystem.calculateHotelPrice(cheapHotel, startDate, endDate));
+		System.out.println(cheapHotel.getName() + ", and TotalRates : "
+				+ hotelReservationSystem.calculateHotelPrice(cheapHotel, startDate, endDate));
 		Assert.assertEquals(hotelReservationSystem.calculateHotelPrice(cheapHotel, startDate, endDate), 200);
 	}
 
@@ -105,11 +108,12 @@ public class HotelTest {
 		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 19);
 
 		Hotel cheapHotel = hotelReservationSystem.findCheapHotelsWithGoodRating(startDate, endDate);
-		System.out.println(cheapHotel.getName() + ", Rating : " + cheapHotel.getRating() + ", and TotalRates : "  + hotelReservationSystem.calculateHotelPrice(cheapHotel, startDate, endDate));
-		
+		System.out.println(cheapHotel.getName() + ", Rating : " + cheapHotel.getRating() + ", and TotalRates : "
+				+ hotelReservationSystem.calculateHotelPrice(cheapHotel, startDate, endDate));
+
 		Assert.assertEquals(hotelReservationSystem.calculateHotelPrice(cheapHotel, startDate, endDate), 200);
 	}
-	
+
 	@Test
 	public void findBestRatedHotelInTheDateRangeTestCase() {
 		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3);
@@ -124,12 +128,12 @@ public class HotelTest {
 		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 19);
 
 		Hotel bestHotel = hotelReservationSystem.findBestRatedHotel(startDate, endDate);
-		System.out.println(bestHotel.getName() + ", and TotalRates : "  + hotelReservationSystem.calculateHotelPrice(bestHotel, startDate, endDate));
-		
+		System.out.println(bestHotel.getName() + ", and TotalRates : "
+				+ hotelReservationSystem.calculateHotelPrice(bestHotel, startDate, endDate));
+
 		Assert.assertEquals(hotelReservationSystem.calculateHotelPrice(bestHotel, startDate, endDate), 370);
 	}
-	
-	
+
 	@Test
 	public void addPriceForRewardCustomers() {
 		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3, 80, 80);
@@ -143,5 +147,57 @@ public class HotelTest {
 
 		Assert.assertEquals(100, hotel2.getRewardCustomerWeekDaysRate());
 	}
-	
+
+	@Test
+	public void findCheapestHotelsWithGoodRatingForRewardCustomerTestCase() {
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3, 80, 80);
+		Hotel hotel2 = new Hotel("RidgeWood", 220, 150, 5, 100, 40);
+		Hotel hotel3 = new Hotel("BridgeWood", 150, 50, 4, 110, 50);
+
+		HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+		hotelReservationSystem.addHotel(hotel1);
+		hotelReservationSystem.addHotel(hotel2);
+		hotelReservationSystem.addHotel(hotel3);
+
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter start date : dd/mm/yyyy : ");
+		String stDate = s.next();
+		System.out.print("Enter end date : dd/mm/yyyy : ");
+		String enDate = s.next();
+
+		System.out.print("Enter 1 for reward customer and 2 for normal customer:  ");
+
+		int customerType = s.nextInt();
+
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+		LocalDate startDate = LocalDate.parse(stDate, dateFormat);
+
+		LocalDate endDate = LocalDate.parse(enDate, dateFormat);
+
+		Hotel hotel = new Hotel();
+		
+		switch (customerType) {
+		case 1:
+			hotel = hotelReservationSystem.findCheapHotelsWithGoodRatingForRewardCustomers(startDate,
+					endDate);
+			System.out.println(hotel.getName() + ", Rating : " + hotel.getRating() + ", and TotalRates : "
+					+ hotelReservationSystem.calculateHotelPriceForRewardCustomers(hotel, startDate, endDate));
+			break;
+
+		case 2:
+			hotel = hotelReservationSystem.findCheapHotelsWithGoodRating(startDate, endDate);
+			System.out.println(hotel.getName() + ", Rating : " + hotel.getRating() + ", and TotalRates : "
+					+ hotelReservationSystem.calculateHotelPrice(hotel, startDate, endDate));
+			break;
+			
+		default:
+			 System.out.println("Enter a valid number");
+			 break;
+		}
+
+		Assert.assertEquals(
+				hotelReservationSystem.calculateHotelPriceForRewardCustomers(hotel, startDate, endDate), 140);
+	}
+
 }
