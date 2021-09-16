9a4f61479ede91a0e7cda22ce1e5680b398bb9df
ajaybarath <ajaybarath3@gmail.com>
2021-09-16 17:30:03 +0530

    ADDED: varibles to store rates for reward customers

--- src/main/java/com/bridgeLabz/hotelReservationSystem/Hotel.java
+++ src/main/java/com/bridgeLabz/hotelReservationSystem/Hotel.java
@@ -6,6 +6,20 @@ public class Hotel {
 	private int weekDaysRate;
 	private int weekEndRate;
 	private int rating;
+	
+	private int rewardCustomerWeekDaysRate;
+	private int rewardCustomerWeekEndRate;
+
+	public Hotel(String name, int weekDaysRate, int weekEndRate, int rating, int rewardCustomerWeekDaysRate,
+			int rewardCustomerWeekEndRate) {
+		super();
+		this.name = name;
+		this.weekDaysRate = weekDaysRate;
+		this.weekEndRate = weekEndRate;
+		this.rating = rating;
+		this.rewardCustomerWeekDaysRate = rewardCustomerWeekDaysRate;
+		this.rewardCustomerWeekEndRate = rewardCustomerWeekEndRate;
+	}
 
 	public Hotel(String name, int weekDaysRate, int weekEndRate, int rating) {
 		super();
@@ -53,6 +67,22 @@ public class Hotel {
 
 	public int getRating() {
 		return rating;
+	}
+
+	public int getRewardCustomerWeekDaysRate() {
+		return rewardCustomerWeekDaysRate;
+	}
+
+	public int getRewardCustomerWeekEndRate() {
+		return rewardCustomerWeekEndRate;
+	}
+
+	public void setRewardCustomerWeekDaysRate(int rewardCustomerWeekDaysRate) {
+		this.rewardCustomerWeekDaysRate = rewardCustomerWeekDaysRate;
+	}
+
+	public void setRewardCustomerWeekEndRate(int rewardCustomerWeekEndRate) {
+		this.rewardCustomerWeekEndRate = rewardCustomerWeekEndRate;
 	}
 	
 	
