932fe2545788d36ce49b7e4f61cd33034cc52b16
ajaybarath <ajaybarath3@gmail.com>
2021-09-16 17:23:07 +0530

    ADDED: test methd to get the best rated hotel

--- src/test/java/com/bridgeLabz/hotelReservationSystem/HotelTest.java
+++ src/test/java/com/bridgeLabz/hotelReservationSystem/HotelTest.java
@@ -109,4 +109,23 @@ public class HotelTest {
 		
 		Assert.assertEquals(hotelReservationSystem.calculateHotelPrice(cheapHotel, startDate, endDate), 200);
 	}
+	
+	@Test
+	public void findBestRatedHotelInTheDateRangeTestCase() {
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3);
+		Hotel hotel2 = new Hotel("RidgeWood", 220, 150, 5);
+		Hotel hotel3 = new Hotel("BridgeWood", 150, 50, 4);
+
+		HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+		hotelReservationSystem.addHotel(hotel1);
+		hotelReservationSystem.addHotel(hotel2);
+		hotelReservationSystem.addHotel(hotel3);
+		LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 17);
+		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 19);
+
+		Hotel bestHotel = hotelReservationSystem.findBestRatedHotel(startDate, endDate);
+		System.out.println(bestHotel.getName() + ", and TotalRates : "  + hotelReservationSystem.calculateHotelPrice(bestHotel, startDate, endDate));
+		
+		Assert.assertEquals(hotelReservationSystem.calculateHotelPrice(bestHotel, startDate, endDate), 370);
+	}
 }
