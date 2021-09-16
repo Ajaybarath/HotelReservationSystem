328cf00abc84c64f3a08983b32f076cd7b49fb6f
ajaybarath <ajaybarath3@gmail.com>
2021-09-16 15:24:46 +0530

    ADDED: function to find cheap hotels and calculate price

--- src/main/java/com/bridgeLabz/hotelReservationSystem/HotelReservationSystem.java
+++ src/main/java/com/bridgeLabz/hotelReservationSystem/HotelReservationSystem.java
@@ -1,5 +1,6 @@
 package com.bridgeLabz.hotelReservationSystem;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.temporal.ChronoUnit;
@@ -36,8 +37,43 @@ public class HotelReservationSystem {
 		Hotel cheapHotel = hotelList.stream().min((n1, n2) -> n1.getWeekDaysRate() - n2.getWeekDaysRate()).orElse(null);
 
 		long noOfDaysBetween = ChronoUnit.DAYS.between(start, end);
-		
+
 		return cheapHotel;
+	}
+
+	public Hotel findCheapHotelsWithWeekDaysAndWeekEndRates(LocalDate start, LocalDate end) {
+
+		Hotel cheapHotel = hotelList.stream().min((n1, n2) -> calculateHotelPrice(n1, start, end)
+				- calculateHotelPrice(n2, start, end)).orElse(null);
+
+		return cheapHotel;
+	}
+
+	
+	public int calculateHotelPrice(Hotel hotel, LocalDate start, LocalDate end) {
+		final int startW = start.getDayOfWeek().getValue();
+	    final int endW = end.getDayOfWeek().getValue();
+
+	    final long days = ChronoUnit.DAYS.between(start, end);
+	    long daysWithoutWeekends = days - 2*(days/7); //remove weekends
+
+	    if (days % 7 != 0) { //deal with the rest days
+	        if (startW == 7) {
+	            daysWithoutWeekends -= 1;
+	        } else if (endW == 7) {  //they can't both be Sunday, otherwise rest would be zero
+	            daysWithoutWeekends -= 1;
+	        } else if (endW < startW) { //another weekend is included
+	            daysWithoutWeekends -= 2;
+	        }
+	    }
+	    long daysWithWeekends = days - daysWithoutWeekends;
+
+		long price = (daysWithoutWeekends * hotel.getWeekDaysRate()) + (daysWithWeekends * hotel.getWeekEndRate());
+
+		System.out.println(price + " " + hotel.getName() + " " + daysWithoutWeekends + " " + daysWithWeekends);
+		return (int) price;
+	    
+	    
 	}
 
 }
