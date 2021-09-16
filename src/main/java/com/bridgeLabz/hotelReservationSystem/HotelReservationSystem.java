13605dc3b99039dc25aa23d3e58097c24b5d8c65
ajaybarath <ajaybarath3@gmail.com>
2021-09-16 09:03:35 +0530

    ADDED: function to add hotel to the list

--- src/main/java/com/bridgeLabz/hotelReservationSystem/HotelReservationSystem.java
+++ src/main/java/com/bridgeLabz/hotelReservationSystem/HotelReservationSystem.java
@@ -1,10 +1,33 @@
 package com.bridgeLabz.hotelReservationSystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HotelReservationSystem {
 	
 	public static void main(String args[]) {
 
 		System.out.println("Welcome to hotel reservation system ");
 	}
+	
+	private List<Hotel> hotelList;
+	
+	public HotelReservationSystem() {
+		hotelList = new ArrayList<>();
+	}
+	
+	public void addHotel(Hotel hotel) {
+		
+		if (hotelList.contains(hotel)) {
+			System.out.println("Hotel already exists");
+		}
+		else {
+			hotelList.add(hotel);
+			System.out.println("Hotel " + hotel.getName() + " added successfully");
+		}
+		
+	}
+	
+	
 
 }
