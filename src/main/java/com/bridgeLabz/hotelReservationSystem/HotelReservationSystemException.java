f478b76a2c5a24cadaa7c6bd1d70620961c83cd8
ajaybarath <ajaybarath3@gmail.com>
2021-09-17 19:12:00 +0530

    ADDED: custom exception class

---
+++ src/main/java/com/bridgeLabz/hotelReservationSystem/HotelReservationSystemException.java
@@ -0,0 +1,9 @@
+package com.bridgeLabz.hotelReservationSystem;
+
+public class HotelReservationSystemException extends Exception {
+
+	public HotelReservationSystemException(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+}
